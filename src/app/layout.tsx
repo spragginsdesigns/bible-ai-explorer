e4682bc22e83a21e212d4789238eecb87e2fa8db
Austin Spraggins <atmosphere9999@gmail.com>
2024-10-04 09:11:53 -0700

    Update layout.tsx
    add Google site verification meta tag to RootLayout for domain ownership verification correctly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,10 +75,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 				<head>
-				<meta
-					name="google-site-verification"
-					content="google031a11ddf3eae0eb.html"
-				/>
+				<meta name="google-site-verification" content="Oz-B3ljjCVJn4t_50kIHVJxEct57K1FqysvU8ZU3beI" />
 			</head>
 			<body className={inter.className}>
 				<ThemeProvider
